70c361d41e7e2de8ffd3a6d2f4b4fa5637b1d978
renatocg82 <66907169+renatocg82@users.noreply.github.com>
2023-07-22 20:38:14 -0300

    Writing with buffered

--- src/application/AppFiWrBufWr.java
+++ src/application/AppFiWrBufWr.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class AppFiWrBufWr {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
 
+		String path = "c:\\temp\\out.txt";
+		
+		try (FileWriter fw = new FileWriter(path, true) ; BufferedWriter bw = new BufferedWriter(fw)) {
+			
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}			
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}	
 	}
-
 }
